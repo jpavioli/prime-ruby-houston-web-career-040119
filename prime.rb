0ef013be30ba005b0ebf67d19ca7fa124348bab6
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-22 19:54:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,13 +3,13 @@ def prime?(number)
   if number < 0
     is_it_prime = FALSE
   else
-    i = 1 
-    until i == number
-      if (number%i != 0)
+    i = 1
+    until i > number
+      if (number%i != 0) & (i!=1)
         is_it_prime = FALSE
-        break
       else
         is_it_prime = TRUE
+        break
       end
       i+=1 
     end
