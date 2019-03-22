8b2d04f885032e9a081fc1220cd7b61afd1ab69e
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-22 20:01:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(number)
   if number <= 1
-    is_it_prime = FALSE
+    is_it_prime = FALSE #1, 0, and any negitive number are not prime numbers
   else
-    i = 2
+    i = 1   #start a counter 
     is_it_prime = TRUE 
     until i == number
-      if (number%i == 0)
+      if (number%i == 0) & (i != 1)
         is_it_prime = FALSE
         break
       end
