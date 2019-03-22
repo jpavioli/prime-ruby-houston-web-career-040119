83f476666869ab06713bd8fdbd399a06cfd9b3c6
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-22 19:53:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  if number < 0
+    is_it_prime = FALSE
+  else
+    i = 1 
+    until i == number
+      if (number%i != 0)
+        is_it_prime = FALSE
+        break
+      else
+        is_it_prime = TRUE
+      end
+      i+=1 
+    end
+  end
+  is_it_prime
+end 
